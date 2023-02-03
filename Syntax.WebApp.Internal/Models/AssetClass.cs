b88b42b2e3563155bb417da8bd6cf6cf102ae8c4
Allan Schramm <3208077+allanschramm@users.noreply.github.com>
2023-02-02 23:53:48 -0300

    Add Models

---
+++ Syntax.WebApp.Internal/Models/AssetClass.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Syntax.Models
+{
+    public class AssetClass
+    {
+        [Key]public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
---
+++ Syntax.WebApp.Internal/Models/AssetPortfolio.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Syntax.Models
+{
+    public class AssetPortfolio
+    {
+        [Key]public int Id { get; set; }
+        public decimal Quantity { get; set; }
+        public decimal PurchasePrice { get; set; }
+        public DateTime Date { get; set; }
+        public string? Type { get; set; }
+
+        public Portfolio Portfolio { get; set; }
+        public Asset Asset { get; set; }
+
+    }
+}
